dc086255bfb513b0e09bc958e92ae3a8950a2daa
pchchv <ipchchv@gmail.com>
2024-12-12 11:40:08 +0300

    Create printfl macro rules

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,23 @@
 //! # Terminal progress bar for Rust
 //!
 //! Console progress bar for Rust.
+// Macro for writing to the giving writer.
+// Used in both pb.rs and multi.rs modules.
+//
+// # Examples
+//
+// ```
+// let w = io::stdout();
+// printfl!(w, "");
+// printfl!(w, "\r{}", out);
+//
+// ```
+macro_rules! printfl {
+    ($w:expr, $($tt:tt)*) => {{
+        $w.write_all(&format!($($tt)*).as_bytes()).ok().expect("write() fail");
+        $w.flush().ok().expect("flush() fail");
+    }}
+}
+
 mod pb;
 mod tty;
