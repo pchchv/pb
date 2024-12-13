2467877512d87a76098de9d36137c9e5fff3fe15
pchchv <ipchchv@gmail.com>
2024-12-13 12:00:43 +0300

    Implement a terminal_size function

---
+++ src/tty/windows.rs
@@ -0,0 +1,14 @@
+use super::{Height, Width};
+
+/// Returns the size of the terminal, if available.
+/// Note that this returns the size of the actual command window,
+/// and not the overall size of the command window buffer
+pub fn terminal_size() -> Option<(Width, Height)> {
+    if let Some((_, csbi)) = get_csbi() {
+        let w: Width = Width((csbi.srWindow.Right - csbi.srWindow.Left) as u16);
+        let h: Height = Height((csbi.srWindow.Bottom - csbi.srWindow.Top) as u16);
+        Some((w, h))
+    } else {
+        None
+    }
+}
