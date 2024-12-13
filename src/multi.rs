fbcb85a0968bfc13a1733922c4500224edb0c678
pchchv <ipchchv@gmail.com>
2024-12-13 12:17:14 +0300

    Create a type MultiBar

--- src/multi.rs
+++ src/multi.rs
@@ -1,4 +1,7 @@
 use std::io::Write;
+use std::sync::Mutex;
+use std::sync::atomic::AtomicUsize;
+use crossbeam_channel::{Receiver, Sender};
 
 struct State<T: Write> {
     lines: Vec<String>,
@@ -12,4 +15,10 @@ struct WriteMsg {
     done: bool,
     level: usize,
     string: String,
+}
+
+pub struct MultiBar<T: Write> {
+    state: Mutex<State<T>>,
+    chan: (Sender<WriteMsg>, Receiver<WriteMsg>),
+    nbars: AtomicUsize,
 }
