c13c597743421976ec99fa5ac012a1ad65176e45
pchchv <ipchchv@gmail.com>
2024-12-13 11:59:10 +0300

    Implement a move_cursor_up function

--- src/tty/wasi.rs
+++ src/tty/wasi.rs
@@ -4,3 +4,8 @@ use super::{Height, Width};
 pub fn terminal_size() -> Option<(Width, Height)> {
     return None;
 }
+
+/// This is inherited from unix and will work only when wasi executed on unix.
+pub fn move_cursor_up(n: usize) -> String {
+    format!("\x1B[{}A", n)
+}
